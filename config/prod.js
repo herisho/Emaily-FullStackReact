e7b57f8afb32ebcdc6962996442df19acceb27fc
kevin <kevin@brounie.com>
2019-02-27 20:51:21 -0600

    DEBUGGING OAuth 0.3

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI:
+    "mongodb+srv://p00gz:5VkciAtFlShFrQMo@emaily-axl6j.mongodb.net/test?retryWrites=true",
   cookieKey: process.env.COOKIE_KEY
 };
