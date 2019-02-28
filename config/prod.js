9d4259112c77112f3563436a93166c5b4eb1c206
kevin <kevin@brounie.com>
2019-02-27 23:23:57 -0600

    Fixing OAuth 0.6

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
   environment: process.env.ENVIRONMENT,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: "mongodb://p00zg:ANf824nasowetn@ds123664.mlab.com:23664/maily-prod",
+  mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
